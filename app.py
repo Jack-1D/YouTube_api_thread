aa199bd310b5a242e801fdae0df04ab37031dc1f
Jack-1D <10635052@mail.hpsh.tp.edu.tw>
2023-06-08 14:13:00 +0800

    remain the select of last time

--- app.py
+++ app.py
@@ -1,17 +1,14 @@
 from flask import Flask, render_template, request, redirect, url_for
 import thread_Youtube
+import json
 app = Flask(__name__)
 @app.route("/",methods=["POST","GET"])
 def hello():
     if request.method == "POST":
-        id = request.form["YouTuber"]
-        content, time = thread_Youtube.run(id)
-        return render_template('result.html',content=content, time=time)
+        dict = json.loads(request.form["YouTuber"])
+        content, time = thread_Youtube.run(dict['id'])
+        return render_template('result.html',content=content, time=time, name=dict['name'])
     return render_template('search.html')
-
-# @app.route("/result")
-# def show_result(content):
-#     return render_template('result.html',content=content)
 
 if __name__ == 'main':
     app.run(host='127.0.0.1', port=5000, debug=False)
